c80ee5fc98c1f26cd59e9ef0669c73e2e6c6ed01
Tiger Mei <tigermei@Tigers-MacBook-Pro.local>
2020-02-01 16:46:14 +1100

    a4 ans

--- CS224N_2020_Winter/a4/nmt_model.py
+++ CS224N_2020_Winter/a4/nmt_model.py
@@ -259,6 +259,7 @@ class NMT(nn.Module):
             Y_t = torch.squeeze(Y_t)
             Ybar_t = torch.cat((Y_t, o_prev), dim=1)    # Why can't it be (o_prev, Y_t) ?
             dec_state, o_t, _ = self.step(Ybar_t, dec_state, enc_hiddens, enc_hiddens_proj, enc_masks)
+            print(enc_masks.shape)
             combined_outputs.append(o_t)
             o_prev = o_t
         combined_outputs = torch.stack(combined_outputs)
